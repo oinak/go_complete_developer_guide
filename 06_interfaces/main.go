91edb06ebab9f37222a33e26fb6f4fdf5dd81ccd
Fernando Martínez <me@oinak.com>
2020-06-10 23:55:45 +0200

    Day 8, Interfaces

--- 06_interfaces/main.go
+++ 06_interfaces/main.go
@@ -2,6 +2,38 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("vim-go")
+type bot interface {
+	getGreeting() string
 }
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func (englishBot) getGreeting() string {
+	// very custom logic for generating english greeting
+	return "Hi there!"
+}
+
+func (spanishBot) getGreeting() string {
+	// very custom logic for generating spanish greeting
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// func printGreeting(eb englishBot) string {
+// 	fmt.Println(eb.getGreeting())
+// }
+
+// func printGreeting(sb spanishBot) string {
+// 	fmt.Println(sb.getGreeting())
+// }
